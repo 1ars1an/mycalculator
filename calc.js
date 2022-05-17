e165337760a20ddfe7afada31bfeec381017cac1
ars1an <arslanmansoor22@gmail.com>
2022-05-17 10:22:20 +0530

    added styling for operator pressed, made new function for resolving buttons

--- calc.js
+++ calc.js
@@ -1,7 +1,10 @@
 var displayValue;
-var currentNumber;
-var operatorFlag;
+var operandOne;
+var operandTwo;
+var operandFlag;
 var currentOperator;
+
+const operators = Array.from(document.querySelectorAll('.operator')); //selects operator buttons
 
 //Functions
 function add(a, b) { 
@@ -17,14 +20,21 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    console.log(operandTwo);
+    if (operandTwo === 0) {
+        return "Error, Can't divide by 0"
+    }
     return a/b;
 }
 
 function clear() {
     displayValue = '';
-    currentNumber = 0;
-    operatorFlag = 0;
+    operandOne = null;
+    operandTwo = null;
+    operandFlag = 0;
     currentOperator = '';
+    operators.forEach(item => item.classList.remove('active'));
+    displayContent();
 }
 
 function displayContent() {
@@ -35,25 +45,93 @@ function displayContent() {
 function operate(operator, a, b) {
     switch(operator) {
         case '+':
-            add(a, b);
+            operandOne = add(a, b);
+            operandTwo = null;
             break; 
         case '-':
-            subtract(a, b);
+            operandOne = subtract(a, b);
+            operandTwo = null;
             break;
         case '*':
-            multiply(a, b);
+            operandOne = multiply(a, b);
+            operandTwo = null;
             break;
         case '/':
-            divide(a, b);
+            operandOne = divide(a, b);
+            operandTwo = null;
             break;
-            
+        case '=':
+            displayValue = operandOne;
+            break;
     }
 }
 
-function setNumbers() {
+function checkString() {
+    if (displayValue === '+' || displayValue === '-' || displayValue === '*' || displayValue === '/') {
+        return true;
+    } else return false;
+}
+
+function setValues() {
+    if (checkString() || operandFlag === 1) {
+        dot.addEventListener('click', setFloat, { once: true });
+        displayValue = '';
+        operandFlag = 0;
+    }
+    displayValue += this.textContent;
+    displayContent();
+}
+
+function setOperation() {
+    if (currentOperator !== '') {
+        if (checkString()) { //if user inputs operator twice in a row, does operation with first number itself [can change this to an error message]
+            displayValue = operandOne;
+        }
+        operandTwo = Number(displayValue);
+        operandFlag = 1;
+        operate(currentOperator, operandOne, operandTwo);
+        currentOperator = this.textContent;
+        displayValue = operandOne; //can put this in switch
+        displayContent();
+    } else {
+    if (this.textContent === '=') {
+        displayValue = 'Error, Equation Not Complete.'
+        displayContent();
+        return;
+    }
+    operandOne = Number(displayValue);
+    currentOperator = this.textContent;
+    displayValue = this.textContent;
+    displayContent();
+    }
+}
+
+function setFloat() {
+    displayValue += this.textContent;
+    displayContent();
+    dot.removeEventListener('click', setFloat);
 }
 
 function selectButton() {
     const numbers = Array.from(document.querySelectorAll('.numbers'));
-    numbers.forEach(item => item.addEventListener('click', setNumbers));
+    numbers.forEach(item => item.addEventListener('click', setValues));
+    operators.forEach(item => item.addEventListener('click', setOperation)); //for operator buttons
+    const clearButton = document.querySelector('.buttons.function');
+    clearButton.addEventListener('click', clear);
+    const dot = document.getElementById('dot');//selects the dot button
+    dot.addEventListener('click', setFloat, { once: true });
 }
+
+function setColor() {
+    operators.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+function colorOperator() {
+    operators.forEach(item => item.addEventListener('click', setColor));
+}
+
+clear();
+displayContent();
+colorOperator();
+selectButton();
